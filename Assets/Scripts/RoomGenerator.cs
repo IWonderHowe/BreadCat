90f43d5c8deead1703165ea82948b096779713f7
Jeff Howe <IWonderHowe@gmail.com>
2024-07-23 18:36:34 -0700

    Made a method that fills an inital array of lists of possible level pieces for each cell in the grid

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -6,18 +6,106 @@ using UnityEngine;
 public class RoomGenerator : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _roomPieces = new List<GameObject>();
-    [SerializeField] private Vector3 _roomSize;
+    [SerializeField] private Vector3Int _roomSize;
     private GameObject[,,] _roomLayout;
+    private List<GameObject>[,,] _availablePieces;
+
+    private void Awake()
+    {
+        // set space for the room layout that is updated with established pieces
+        _roomLayout = new GameObject[_roomSize.x, _roomSize.y, _roomSize.z];
+
+        // set space for the available room pieces in vacant spaces
+        _availablePieces = new List<GameObject>[_roomSize.x, _roomSize.y, _roomSize.z];
+
+        FillAvailableRoomPieces();
+        Debug.Log(_availablePieces.GetLength(0));
+
+        for (int x = 0; x < _roomSize.x; x++)
+        {
+            for (int y = 0; y < _roomSize.y; y++)
+            {
+                for (int z = 0; z < _roomSize.z; z++)
+                {
+                    Debug.Log(_availablePieces[x, y, z].Count + ": at coordinate " + x + ", " + y + ", " + z);
+                }
+            }
+        }
+    }
 
     public void GenerateRoom()
     {
-        _roomLayout = new GameObject[(int)_roomSize.x,(int)_roomSize.y,(int)_roomSize.z];
         int[] startCell = FindStartCell("North");
         //Debug.Log(roomLayout.GetLength(0) + " " + roomLayout.GetLength(1) + " " + roomLayout.GetLength(2));
         SpawnRoomPiece(startCell);
         GetNextCell(startCell);
 
     }
+
+    private void FillAvailableRoomPieces()
+    {
+        // fill the available pieces array with all possible pieces
+        for (int x = 0; x < _roomSize.x; x++)
+        {
+            for (int y = 0; y < _roomSize.y; y++)
+            {
+                for (int z = 0; z < _roomSize.z; z++)
+                {
+                    _availablePieces[x, y, z] = new List<GameObject>();
+                    // add each room piece to possible pieces
+                    foreach (GameObject roomPiece in _roomPieces)
+                    {
+                        // omit room piece being added if it is on the edge of the room layout and the outside wall doesnt exist
+                        if (x == 0 && roomPiece.GetComponent<LevelPiece>().WestOpen) continue;
+                        if (x == _roomSize.x - 1 && roomPiece.GetComponent<LevelPiece>().EastOpen) continue;
+                        if (z == 0 && roomPiece.GetComponent<LevelPiece>().SouthOpen) continue;
+                        if (z == _roomSize.z - 1 && roomPiece.GetComponent<LevelPiece>().NorthOpen) continue;
+
+                        // omit room entrance or exit piece if the current gridspot is not on a room edge
+                        if (x == 0 || x == _roomSize.x - 1 || z == 0 || z == _roomSize.z - 1)
+                        {
+                            if (roomPiece.GetComponent<LevelPiece>().IsEntrance || roomPiece.GetComponent<LevelPiece>().IsExit) continue;
+                        }
+
+                        _availablePieces[x, y, z].Add(roomPiece);
+                    }
+                }
+            }
+        }
+    }
+
+    private List<Vector3Int> GetAdjacentEmptyCells(int[] originCell)
+    {
+        List<Vector3Int> adjacentCells = new List<Vector3Int>();
+
+        // check all adjacjent cells to the origin cell, if they exist and are empty add them to the list of the available cells
+        // check cells to the left and right of starter cell
+        if (originCell[0] - 1 >= 0 && _roomLayout[originCell[0] - 1, originCell[1], originCell[2]] == null)
+        {
+            adjacentCells.Add(new Vector3Int(originCell[0] - 1, originCell[1], originCell[2]));
+        }
+        if (originCell[0] + 1 < _roomLayout.GetLength(0) && _roomLayout[originCell[0] + 1, originCell[1], originCell[2]] == null)
+        {
+            adjacentCells.Add(new Vector3Int(originCell[0] + 1, originCell[1], originCell[2]));
+        }
+        // check cells ahead of and behind the starter cell
+        if (originCell[2] - 1 >= 0 && _roomLayout[originCell[0], originCell[1], originCell[2] - 1] == null)
+        {
+            adjacentCells.Add(new Vector3Int(originCell[0], originCell[1], originCell[2] - 1));
+        }
+        if (originCell[2] + 1 < _roomLayout.GetLength(2) && _roomLayout[originCell[0], originCell[1], originCell[2] + 1] == null)
+        {
+            adjacentCells.Add(new Vector3Int(originCell[0], originCell[1], originCell[2] + 1));
+        }
+
+        return adjacentCells;
+    }
+
+    private void UpdateAvailableRoomPieces(GameObject previousPlacedPiece)
+    {
+
+    }
+
 
     private int[] GetNextCell(int[] lastCell)
     {
@@ -28,17 +116,9 @@ public class RoomGenerator : MonoBehaviour
         List<Vector3Int> availableCells = new List<Vector3Int>();
 
 
-        // check all adjacjent cells to the starter cell, if they exist and are empty add them to the list of the available cells
-        // check cells to the left and right of starter cell
-        if (lastCell[0] - 1 >= 0 && _roomLayout[lastCell[0] - 1, lastCell[1], lastCell[2]] == null)
-            availableCells.Add(new Vector3Int(lastCell[0] - 1, lastCell[1], lastCell[2]));
-        if (lastCell[0] + 1 < _roomLayout.GetLength(0) && _roomLayout[lastCell[0] + 1, lastCell[1], lastCell[2]] == null)
-            availableCells.Add(new Vector3Int(lastCell[0] + 1, lastCell[1], lastCell[2]));
-        // check cells ahead of and behind the starter cell
-        if (lastCell[2] - 1 >= 0 && _roomLayout[lastCell[0], lastCell[1], lastCell[2] - 1] == null)
-            availableCells.Add(new Vector3Int(lastCell[0], lastCell[1], lastCell[2] - 1));
-        if (lastCell[2] + 1 < _roomLayout.GetLength(2) && _roomLayout[lastCell[0], lastCell[1], lastCell[2] + 1] == null)
-            availableCells.Add(new Vector3Int(lastCell[0], lastCell[1], lastCell[2] + 1)); 
+        
+        availableCells = GetAdjacentEmptyCells(lastCell);
+
 
         foreach(Vector3Int cell in availableCells)
         {
