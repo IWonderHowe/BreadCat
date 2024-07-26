7117b3cef623f4290bfb374fe2ac401469e8ea44
Jeff Howe <IWonderHowe@gmail.com>
2024-07-25 17:27:36 -0700

    made a rudimentary player spawner

---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    [SerializeField] GameObject _playerContainer;
+
+    private void Awake()
+    {
+        Instantiate(_playerContainer);
+    }
+}
