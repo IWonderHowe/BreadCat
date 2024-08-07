e673ba3c99e29c994f2cfc9cea2e4fd9c28516af
Jeff Howe <IWonderHowe@gmail.com>
2024-08-07 14:19:59 -0700

    Commented recently made scripts

--- Assets/Scripts/Characters/EnemyCombat.cs
+++ Assets/Scripts/Characters/EnemyCombat.cs
@@ -9,77 +9,91 @@ public class EnemyCombat : MonoBehaviour
     // in idle state: Check to see if the player is in visual range of the enemy (or check to see if got hit by player)
     // in combat state: Move to a good position, shoot, idle, repeat
 
+    // get relevant layers for sightlines
     [SerializeField] private LayerMask _playerLayer;
+    [SerializeField] private LayerMask _visionLayers;
+
+    // get gun and relevant properties
     [SerializeField] private float _timeBetweenShots;
     [SerializeField] private EnemyGun _gun;
     [SerializeField] private float _sightRange;
-    [SerializeField] private LayerMask _raycastIgnoreLayer;
     [SerializeField] private GameObject _bulletOrigin;
-    [SerializeField] private GameObject _target;
 
+    // set a space aside to store the current target and whether LoS is established
+    [SerializeField] private GameObject _target;
+    private bool _hasTargetLoS = false;
+    
+    // set whether this enemy can move
     [SerializeField] private bool _canMove = false;
+
 
     private Coroutine _currentCoroutine;
 
-    private bool _hasPlayerLoS = false;
 
     private void Start()
     {
+        // begin enemy in their idle state
         _currentCoroutine = StartCoroutine(IdleState());
-
-
-
     }
 
     private void FixedUpdate()
     {
         //Debug.Log(CheckPlayerLoS());
-        _hasPlayerLoS = CheckPlayerLoS();
+        // see if the enemy can currently see the player
+        _hasTargetLoS = CheckTargetLoS();
 
         // look at the player if in LoS
-        if (_hasPlayerLoS) SetEnemyFacing(transform.position - _target.transform.position);
+        if (_hasTargetLoS) SetEnemyFacing(transform.position - _target.transform.position);
     }
 
 
     private IEnumerator IdleState()
     {
         Debug.Log("Started idle state");
-        while(!_hasPlayerLoS)
+        // do nothing while the enemy does not have LoS with target
+        while(!_hasTargetLoS)
         {   
             yield return null;
         }
+
+        // when LoS is established, enter the aggro state
         StartCoroutine(AggroState());
     }
 
     private IEnumerator AggroState()
     {
         Debug.Log("Started aggro state");
-        while (_hasPlayerLoS)
+
+        // while the enemy has LoS on the target
+        while (_hasTargetLoS)
         {
             // if the enemy can move, do movement stuffs
             if (_canMove) ;
 
-            SetEnemyFacing(transform.position - _target.transform.position);
-
-
+            // shoot at the target then wait for the proper time between shots
             _gun.Shoot(_target);
             yield return new WaitForSeconds(_timeBetweenShots);
         }
+
+        // upon breaking LoS, return to the idle state
         StartCoroutine(IdleState());
     }
 
     private void SetEnemyFacing(Vector3 direction)
     {
+        // Set the look rotation to be towards the target, then flatten the look direction
         Vector3 lookRotation = Quaternion.LookRotation(_target.transform.position - transform.position).eulerAngles;
         lookRotation.x = 0;
         lookRotation.z = 0;
 
+        // set the current rotation to be that of the look rotation
         transform.rotation = Quaternion.Euler(lookRotation);
 
     }
 
-    private bool CheckPlayerLoS()
+    private bool CheckTargetLoS()
     {
+        // store a spot to store hit data
         RaycastHit hit;
 
         // get direction of player in relation to the bullet origin
@@ -87,7 +101,7 @@ public class EnemyCombat : MonoBehaviour
 
         
         // if the object hit by the raycast is the player, return true
-        if(Physics.Raycast(_bulletOrigin.transform.position, directionOfPlayer, out hit, _sightRange, _raycastIgnoreLayer))
+        if(Physics.Raycast(_bulletOrigin.transform.position, directionOfPlayer, out hit, _sightRange, _visionLayers))
         {
             if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
             {
@@ -100,14 +114,13 @@ public class EnemyCombat : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        // show a blue line for enemy LoS
         Vector3 directionOfPlayer = _target.transform.position - _bulletOrigin.transform.position;
-        if (Physics.Raycast(_bulletOrigin.transform.position, directionOfPlayer, out RaycastHit hit,  _sightRange, _raycastIgnoreLayer))
+        if (Physics.Raycast(_bulletOrigin.transform.position, directionOfPlayer, out RaycastHit hit,  _sightRange, _visionLayers))
         {
             Gizmos.color = Color.blue;
             Gizmos.DrawLine(hit.point, _bulletOrigin.transform.position);
 
         }
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(_target.transform.position, this.gameObject.transform.position);
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    // set space for all relevant parent colliders and current collider
     private Collider _projectileCollider;
     private GameObject _parentObject;
-
     private List<Collider> _collidersToIgnore = new List<Collider>();
 
     private void Awake()
     {
+        // set the projectile collider to be this collider
         _projectileCollider = GetComponent<Collider>();
     }
 
+
     private void OnTriggerEnter(Collider collision)
     {
+        // if triggered by player, hit the player
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("hit player");
         }
+
+        // destroy projectile when it hits another collider
         Destroy(this.gameObject);
     }
 
@@ -32,9 +37,13 @@ public class Projectile : MonoBehaviour
         Destroy(this.gameObject);
     }
 
+
     public void SetParentObject(GameObject parent) 
     {
+        // set the parent of the projectile
         _parentObject = parent;
+
+        // ignore all colliders in the parent for this projectile
         foreach (Collider parentCollider in GetComponentsInChildren<Collider>())
         {
             _collidersToIgnore.Add(parentCollider);
